34ed05db0eadc6cf30047b10096e9476bf1e9490
azadbcl <52546241+akazad7567@users.noreply.github.com>
2024-02-12 13:43:15 +0600

    removed warning

--- src/validate/db-connection.ts
+++ src/validate/db-connection.ts
@@ -1,41 +1,34 @@
-import { MongoClient } from "mongodb";
-import { ConfigService } from "../services/service.config";
+import { MongoClient } from 'mongodb';
+import { ConfigService } from '../services/service.config';
 
 const config = ConfigService.getInstance().getConfig();
 
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useFindAndModify: false,
-    // useCreateIndex: true,
-  };
+export class MongoGlobal {
+  private static mongo: MongoGlobal;
+  private mongoClient: any;
+  private constructor() {
+    this.mongoClient = MongoClient;
+  }
 
-export class MongoGlobal{
-    private static mongo : MongoGlobal;
-    private mongoClient: any;
-    private constructor(){
-        this.mongoClient = MongoClient;
+  public static getMongoInstance() {
+    if (!MongoGlobal.mongo) {
+      MongoGlobal.mongo = new MongoGlobal();
     }
+    return MongoGlobal.mongo;
+  }
 
-    public static getMongoInstance(){
-        if( !MongoGlobal.mongo ){
-            MongoGlobal.mongo = new MongoGlobal();
-        }
-        return MongoGlobal.mongo;
-    };
-
-
-    public async getMongoConnection(){
-        return new Promise((resolve, reject)=>{
-            this.mongoClient.connect(config.mongo.url, options).then(async(client: any)=>{
-                resolve(client);
-            }).catch((error: any)=>{
-                reject (error);
-            });
-        }).catch((e) => {
-            console.log('getMongoConnection catch error: ', e);
-        });;
-    }
-
-
+  public async getMongoConnection() {
+    return new Promise((resolve, reject) => {
+      this.mongoClient
+        .connect(config.mongo.url)
+        .then(async (client: any) => {
+          resolve(client);
+        })
+        .catch((error: any) => {
+          reject(error);
+        });
+    }).catch((e) => {
+      console.log('getMongoConnection catch error: ', e);
+    });
+  }
 }
